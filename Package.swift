2390159f7a68f2f8677a69be002197787038ca93
t-osawa-009 <da87435@gmail.com>
2020-09-10 18:36:13 +0900

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "Gecco",
+    platforms: [
+        .iOS(.v8)
+    ],
+    products: [
+        .library(name: "Gecco", targets: ["Gecco"]),
+    ],
+    targets: [
+        .target(
+            name: "Gecco",
+            path: "Classes"
+        ),
+         .testTarget(
+            name: "GeccoTests",
+            dependencies: ["Gecco"],
+            path: "GeccoTests"
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+)
